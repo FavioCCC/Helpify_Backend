8155a40615153e592c1e781eb2c9c074696cea33
Maria José Elorrieta Carrión <u202311409@upc.edu.pe>
2025-09-29 21:31:35 -0500

    DTOs siguientes

---
+++ src/main/java/com/webcrafters/helpify/DTO/ComentarioSoloDatosDTO.java
@@ -0,0 +1,24 @@
+package com.webcrafters.helpify.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ComentarioSoloDatosDTO {
+    private Long idcomentario;
+
+    @NotBlank(message = "El comentario del proyecto no puede estar vacío")
+    @Size(min = 50, max = 500, message = "El comentario del proyecto debe tener entre 50 y 500 caracteres")
+    private String comentario;
+
+    @NotNull(message = "El puntaje en estrellas del proyecto no puede estar vacío")
+    private double estrella;
+}
---
+++ src/main/java/com/webcrafters/helpify/DTO/NotificacionSoloDatosDTO.java
@@ -0,0 +1,24 @@
+package com.webcrafters.helpify.DTO;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificacionSoloDatosDTO {
+    private Long idnotificacion;
+    @Size(min = 50, max = 250, message = "El notificación del proyecto debe tener entre 50 y 250 caracteres")
+    private String mensaje;
+    @Size(max = 50, message = "La notificación del proyecto debe tener menos de 50 caracteres")
+    private String tipo;
+    private LocalDate fechaEnvio;
+    private Boolean leido;
+}
+
