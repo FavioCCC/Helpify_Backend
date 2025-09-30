6ac714cfdfbc3ce85494afddf69cb50ddde5b63a
FavioCCC <u202310792@upc.edu.pe>
2025-09-29 20:33:22 -0500

    Initial commit

---
+++ src/test/java/com/webcrafters/helpify/HelpifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webcrafters.helpify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
