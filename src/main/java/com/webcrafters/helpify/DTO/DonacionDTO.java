3c5d1e399dbaca6b0f40d9cda204bc9883f084ed
Marco Alejandro Del Pozo Rossi <u202311516@upc.edu.pe>
2025-09-29 21:41:10 -0500

    Implementacion de DTOS restantes

---
+++ src/main/java/com/webcrafters/helpify/DTO/DonacionDTO.java
@@ -0,0 +1,25 @@
+package com.webcrafters.helpify.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DonacionDTO {
+    private Long id;
+    private LocalDate fechadonacion;
+    private BigDecimal montototal;
+    private String estado;
+
+    private UsuarioSoloConDatosDTO usuario;
+    private ProyectoSoloConDatosDTO proyecto;
+
+
+}
