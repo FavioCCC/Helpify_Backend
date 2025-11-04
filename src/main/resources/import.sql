aaff9da2b9524fb23e805b3afad41b7ac8c71143
Marco Alejandro Del Pozo Rossi <u202311516@upc.edu.pe>
2025-11-04 01:06:10 -0500

    Insercion de nuevo usuario en import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,19 @@
-//Base de datos inicial
+/*Base de datos inicial*/
 INSERT INTO rol (nombre) VALUES ('ROLE_ADMIN');
 INSERT INTO rol (nombre) VALUES ('ROLE_VOLUNTARIO');
 INSERT INTO rol (nombre) VALUES ('ROLE_DONANTE');
 
 
-//Usuarios
+/*Usuarios*/
 INSERT INTO usuario (numerodocumento, nombredocumento, nombre, apellidopaterno, apellidomaterno, celular, correo, password, fecharegistro, idrol) VALUES ('87654321', 'DNI', 'Marco', 'Del Pozo', 'Rossi', '888888888', 'marco@email.com', '$2a$12$xTrWRVqMbG905Slsmo4wb.J6Tl3JYLM3Agp8wAveSXIO8omegfkTq', NOW(), 1);
 
 INSERT INTO usuario (numerodocumento, nombredocumento, nombre, apellidopaterno, apellidomaterno, celular, correo, password, fecharegistro, idrol) VALUES ('12345678', 'DNI', 'Juan', 'Perez', 'Lopez', '999999999', 'juan@email.com', '$2a$12$fwy1GDh.YYwYXnZXa4gIfuQIQuwkuY8u7e5isB/1HsykxnNaxyKa2', NOW(), 2);
 
-//Proyectos
+/*majo 12345*/
+INSERT INTO usuario (numerodocumento, nombredocumento, nombre, apellidopaterno, apellidomaterno, celular, correo, password, fecharegistro, idrol) VALUES ('87456321', 'DNI', 'Majo', 'Elorrieta', 'Carrion', '111111111', 'majo@email.com', '$2a$12$DvNJV8MBq2/acL/3Iu3LLeEnwd0P1COlGBUquZZH4N6ZN2Swc7frO', NOW(), 3);
+
+
+/*Proyectos*/
 INSERT INTO proyectos (nombreproyecto, descripcion, montoobjetivo, montorecaudado, fechainicio, fechafin, nombreorganizacion, escuelabeneficiada, cupoMaximo, imagen) VALUES ('Biblioteca Escolar', 'Construcción y equipamiento de una biblioteca para la escuela local.', 15000.00, 1200.50, '2025-01-10', '2025-06-30', 'Fundacion Cultura Viva', 'Escuela Primaria N°12', 200, 'https://otramirada.pe/sites/default/files/https%3A/otramirada.pe/sites/default/files/filefield_paths/educ.rural_.jpeg/educ.rural_.jpeg');
 
 INSERT INTO proyectos (nombreproyecto, descripcion, montoobjetivo, montorecaudado, fechainicio, fechafin, nombreorganizacion, escuelabeneficiada, cupoMaximo, imagen) VALUES ('Huerto Comunitario', 'Proyecto para crear un huerto educativo y sostenible.', 5000.00, 800.00, '2025-03-01', '2025-09-01', 'Red Verde', 'Escuela Secundaria Nº3', 50, 'https://gobernanzadelatierra.org.pe/wp-content/uploads/2017/07/rural-450x400.png');
@@ -20,7 +24,7 @@ INSERT INTO proyectos (nombreproyecto, descripcion, montoobjetivo, montorecaudad
 
 INSERT INTO proyectos (nombreproyecto, descripcion, montoobjetivo, montorecaudado, fechainicio, fechafin, nombreorganizacion, escuelabeneficiada, cupoMaximo, imagen) VALUES ('Programa de Becas', 'Fondo para becas escolares y materiales didácticos.', 20000.00, 5600.75, '2025-01-20', '2026-01-20', 'Manos Solidarias', 'Varias escuelas municipales', 20, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0lCeNJQ5XkmbVzj0HZdBJyYG5kNvydtaiPA&s');
 
-//Comentarios
+/*Comentarios*/
 INSERT INTO comentario (comentario, estrella, idusuario) VALUES ('Excelente iniciativa, me encantó apoyar.', 4.5, (SELECT idusuario FROM usuario WHERE correo = 'marco@email.com'));
 
 //Notificacion
