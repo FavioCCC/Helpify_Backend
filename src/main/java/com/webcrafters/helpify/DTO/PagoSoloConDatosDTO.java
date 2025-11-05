f4659dc9bad1760056be5734929f5da67cb850ae
FavioCCC <u202310792@upc.edu.pe>
2025-11-05 01:48:08 -0500

    Creación de archivo PagoSoloConDatosDTO.java

---
+++ src/main/java/com/webcrafters/helpify/DTO/PagoSoloConDatosDTO.java
@@ -0,0 +1,38 @@
+package com.webcrafters.helpify.DTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PagoSoloConDatosDTO {
+    private Long idPago;
+
+    @NotNull(message = "El monto es obligatorio")
+    private BigDecimal monto;
+
+    private LocalDate fechapago;
+
+    @NotBlank(message = "El número de tarjeta es obligatorio")
+    private String numerotarjeta;
+
+    @NotBlank(message = "El nombre del titular es obligatorio")
+    private String nombretitular;
+
+    @NotNull(message = "La fecha de expiración es obligatoria")
+    private LocalDate fechaexpiracion;
+
+    @NotBlank(message = "El CVV es obligatorio")
+    @Size(min = 3, max = 3, message = "Datos de tarjeta inválidos. Verifique la información e intente nuevamente")
+    private String cvv;
+}
